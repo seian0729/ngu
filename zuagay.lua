2e42d164db7f5d0eb0beb59cadde94ef51eb8629
chimnguu <57534799+chimnguu@users.noreply.github.com>
2021-06-10 15:04:22 +0700

    Update zuagay.lua

--- zuagay.lua
+++ zuagay.lua
@@ -5,8 +5,8 @@ local Setting = {
 print(Setting[1])
 local buoi = [[
 getgenv().Key = "]].. Setting[1]..'"'..[[
+loadstring('https://raw.githubusercontent.com/chimnguu/ngu/master/zuagay.lua')()
+]]
 local Exploit = secure_load and "Sentinel" or syn and "Synapse"
 loadstring(game:HttpGet("https://ego-d.xyz/Script?Exploit="..Exploit))()
-]]
-
 syn.queue_on_teleport(buoi)
