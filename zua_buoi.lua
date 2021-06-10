459e97b097f31eb6c9fcd5f970b1ac06f9da3b45
chimnguu <57534799+chimnguu@users.noreply.github.com>
2021-06-10 15:47:36 +0700

    Update zua_buoi.lua

--- zua_buoi.lua
+++ zua_buoi.lua
@@ -9,22 +9,9 @@ if buoi_cua_rua then
     Buoi["Product Key"] = getgenv().Key 
 	writefile("Zua_hap_key.json", game:GetService("HttpService"):JSONEncode(Buoi))
 end
-loadstring(game:HttpGet('https://raw.githubusercontent.com/chimnguu/ngu/master/zua_buoi.lua'))()
 local Exploit = secure_load and "Sentinel" or syn and "Synapse"
 loadstring(game:HttpGet("https://ego-d.xyz/Script?Exploit="..Exploit))()
 local buoi = [[
-local buoi_cua_rua = isfile("Zua_hap_key.json")
-if buoi_cua_rua then
-    local Buoi = Https:JSONDecode(readfile("Zua_hap_key.json"))
-    getgenv().Key = Buoi["Product Key"]
-    
-    else
-    local Buoi = {}
-    Buoi["Product Key"] = getgenv().Key 
-	writefile("Zua_hap_key.json", game:GetService("HttpService"):JSONEncode(Buoi))
-end
-local Exploit = secure_load and "Sentinel" or syn and "Synapse"
-loadstring(game:HttpGet("https://ego-d.xyz/Script?Exploit="..Exploit))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/chimnguu/ngu/master/zua_buoi.lua'))()
 ]]
 
